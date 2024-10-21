ca40f5f08448e1bce6999bc59328cb48f8dde1dd
SongJungHyun1004 <201802112@o.cnu.ac.kr>
2024-10-21 15:34:20 +0900

    init

---
+++ src/main/java/com/nhnacademy/heukbaekbookshop/HeukbaekbookShopApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.heukbaekbookshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeukbaekbookShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeukbaekbookShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhnacademy/heukbaekbookshop/HeukbaekbookShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.heukbaekbookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeukbaekbookShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
