cad78405629353f8a42270c304038014a77af666
banbakbulga <junh9126@gmail.com>
2024-11-13 02:10:21 +0900

    [Style] 불필요한 주석 제거

--- src/main/java/com/nhnacademy/heukbaekbookshop/order/domain/OrderRepository.java
+++ src/main/java/com/nhnacademy/heukbaekbookshop/order/domain/OrderRepository.java
@@ -4,5 +4,4 @@ import com.nhnacademy.heukbaekbookshop.order.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    // 필요한 커스텀 메서드가 있다면 추가
 }
