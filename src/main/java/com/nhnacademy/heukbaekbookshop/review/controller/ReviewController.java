2a88df932040dab20ea29bdda71be8700b36a8ad
banbakbulga <junh9126@gmail.com>
2024-11-21 15:11:43 +0900

    Merge branch 'develop' of https://github.com/nhnacademy-be7-heukbaekbook/heukbaekbook-shop into feature/review

--- src/main/java/com/nhnacademy/heukbaekbookshop/review/controller/ReviewController.java
+++ src/main/java/com/nhnacademy/heukbaekbookshop/review/controller/ReviewController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/reviews")
-public class ReviewController {
+public class ReviewController {fg
     private final ReviewService reviewService;
 
     public ReviewController(ReviewService reviewService) {
